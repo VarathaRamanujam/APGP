754e6d609a4b9a5d9f52ce31cc2adfa4bdb7612c
C VARATHA RAMANUJAM <108256871+VarathaRamanujam@users.noreply.github.com>
2022-08-26 18:09:03 +0530

    Uploaded the Armstrong number problem

--- Series_APGP.java
+++ Series_APGP.java
@@ -1,26 +1,47 @@
 package My_Basic_programs;
 
 public class Series_APGP {
+	//int a=10;
 
 	public static void main(String[] args) {
 		Series_APGP obj = new Series_APGP();
-		obj.Series_1();//Find the next term in the series : 6, 7, 9, 13, 21,_(1,2,4,8,16
-		obj.Series_2();//Find the next term in the series :1,2,4,7,11,16,22,_(1,2,3,4,5,
-		obj.Series_3();////Find the next term in the series :1,2,5,10,17,26,37(1,3,5,7,9,
-	        obj.Series_4();//Find the next term in the series :46,37,29,22,16,11,7,4,2,1(reverse of series_3)
-	        obj.Series_5();//Find the next term in the series :1,3,7,13,21,31,_(2,4,6,8,10,12
-		obj.Series_6();//Find the next term in the series :1 9 17 33 49 73 97 129 161,_(8,8,16,16,24,24,32,32
-		obj.Series_7();//Find the next term in the series :1 3 6 10 15 21 28 36 45 55 __(2,3,4,5,6....
-/               obj.Series_8();//Find the next term in the series :1 2 6 24 120 720 5040 40320 362880___(*2,*3,*4,*5....
-                obj.Series_9();//Find the next term in the series :3 8 19 42 89
+		//System.out.print(obj.a);
+		//obj.Series_1();//Find the next term in the series : 6, 7, 9, 13, 21,_(1,2,4,8,16
+		//obj.Series_2();//Find the next term in the series :1,2,4,7,11,16,22,_(1,2,3,4,5,
+		//obj.Series_3();////Find the next term in the series :1,2,5,10,17,26,37(1,3,5,7,9,
+	    //obj.Series_4();//Find the next term in the series :46,37,29,22,16,11,7,4,2,1(reverse of series_3)
+		//obj.Series_5();//Find the next term in the series :1,3,7,13,21,31,_(2,4,6,8,10,12
+		//obj.Series_6();//Find the next term in the series :1 9 17 33 49 73 97 129 161,_(8,8,16,16,24,24,32,32
+//		  obj.Series_7();//Find the next term in the series :1 3 6 10 15 21 28 36 45 55 __(2,3,4,5,6....
+//        obj.Series_8();//Find the next term in the series :1 2 6 24 120 720 5040 40320 362880___(*2,*3,*4,*5....
+//       obj.Series_9();//Find the next term in the series :3 8 19 42 89
+		obj.Armstrong_Number();
+	}
+
+	private void Armstrong_Number() {
+		int a=153;
+		int b=a;
+		int arm=0;
+		int sum=0;
+		while(a>0) {
+			sum=a%10;
+			a=a/10;
+			
+			arm=(sum*sum*sum)+arm;			
+		}
+		if (arm==b) {
+			System.out.println("Its Armstrong_Number");
+		}else
+			System.out.println("Its not a Armstrong_Number");
 	}
 
 	private void Series_9() {//Find the next term in the series :3 8 19 42 89 184 375__ (
+		//System.out.println(a);
 		int n=10;
 		int sum=1;
 		for(int i=1;i<=n;i++) {
 			sum=(sum*2)+i;
-			System.out.print(sum+" ");
+			//System.out.print(sum+" ");
 		}
 	}
 
